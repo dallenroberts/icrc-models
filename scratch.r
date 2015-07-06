379beaf0ef5cee18ea89f78f26c2a487fa7325f5
unknown <dallenr@MXL24705MR_ICRC.clients.nebula2.washington.edu>
2015-07-06 15:57:15 -0700

    Restructured disease progression function using merges rather than fast joins.  Added VL efflux component.

--- scratch.r
+++ scratch.r
@@ -153,29 +153,41 @@ agePop <- function(dt) {
 ## Disease progression
 progressDisease <- function(dt) {
   
-  # dt <- copy(pop)
-  
   ## Note that we want the probability of progressing in a given time step.  This can be obtained from the mean duration using the exponential decay function (assuming a constant rate).  http://hyperphysics.phy-astr.gsu.edu/hbase/nuclear/meanlif.html
   
   setkey(dis_prog, hiv, male, vl, cd4)
   setkey(dt, hiv, male, vl, cd4)
   
-  ## CD4 Efflux
-  dt[dis_prog[cd4 < 5], diff := diff - count * exp(1 - 1/cd4_duration)]
-  l
-  ## VL Efflux
-  dt[dis_prog[vl < 5], diff := diff - count * exp(1 - 1/vl_duration)]
+  dt <- merge(dt, dis_prog, all.x = TRUE)
   
-  ## Influx - need to get previous categories
+  ## CD4 Efflux
+  dt[!is.na(cd4_duration), diff := diff - count * exp(1 - 1/cd4_duration)]
+  
+  ## VL Efflux
+  dt[!is.na(vl_duration), diff := diff - count * exp(1 - 1/vl_duration)]
+  
+  ## Influx - need to get counts from previous CD4 and VL categories, calculate how many people leave the category, increment the category by 1, and merge back to the original data frame and add that number of people.  Probably a cleaner way of doing this
   ## CD4 Influx
-  prev_cd4 <- copy(dt[cd4 > 0 & cd4 < 5 & hiv == 1])
-  prev_cd4[dis_prog[cd4 < 5], c("prob", "count_prev") := list(exp(1 - 1/cd4_duration), count)]
+  prev_cd4 <- copy(dt[vl != 0 & cd4 > 0 & cd4 < 5 & hiv == 1])
+  prev_cd4[, cd4_influx := count * exp(1 - 1/cd4_duration)]
   prev_cd4[, cd4 := cd4 + 1]
+  prev_cd4[, c("count", "cd4_duration", "vl_duration", "diff") := NULL]
   setkeyv(prev_cd4, all_keys)
   setkeyv(dt, all_keys)
-  dt[prev_cd4, diff := diff + prob * count_prev]
+  dt <- merge(dt, prev_cd4, all.x = TRUE)
+  dt[!is.na(cd4_influx), diff := diff + cd4_influx]
+  dt[, cd4_influx := NULL]
   
-  ## VL Influx (check that above code actually works first)
+  ## VL Influx
+  prev_vl <- copy(dt[cd4 != 0 & vl > 0 & vl < 5 & hiv == 1])
+  prev_vl[, vl_influx := count * exp(1 - 1/vl_duration)]
+  prev_vl[, vl := vl + 1]
+  prev_vl[, c("count", "cd4_duration", "vl_duration", "diff") := NULL]
+  setkeyv(prev_vl, all_keys)
+  setkeyv(dt, all_keys)
+  dt <- merge(dt, prev_vl, all.x = TRUE)
+  dt[!is.na(vl_influx), diff := diff + vl_influx]
+  dt[, c("vl_influx", "cd4_duration", "vl_duration") := NULL]
   
 }
 
