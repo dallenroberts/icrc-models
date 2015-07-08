2e40e2d162e12bc5839228a4441156ad38eecbad
unknown <dallenr@MXL24705MR_ICRC.clients.nebula2.washington.edu>
2015-07-08 10:44:29 -0700

    Moved mixing matrix generation and lambda parameter loading.

--- load_parameters.r
+++ load_parameters.r
@@ -47,8 +47,37 @@ setkey(hiv_mort, hiv, age, cd4)
 dis_prog <- fread("data/disease_progression.csv")
 dis_prog$hiv <- 1
 
-## Initialize mixing matrix - can we do this just once or does it need to be sex specific?.  Male is only entered in once because all partnerships are assumed to be heterosexual, so male only identifies the individual (the partner is abs(male - 1))
-mix_mat <- as.data.table(expand.grid(male, age, risk, age, risk))
+## Initialize mixing matrix.
+mix_mat <- as.data.table(expand.grid(male, age, risk, male, age, risk))
+
 ## The "_p" indicates that the attribute corresponds to the individual's partner
-setattr(mix_mat, 'names', c("male", "age", "risk", "age_p", "risk_p"))
+setattr(mix_mat, 'names', c("male", "age", "risk", "male_p", "age_p", "risk_p"))
+
+## Assuming only heterosexual transmission
+mix_mat <- mix_mat[mix_mat$male != mix_mat$male_p]
+
 mix_mat$prop <- 0
+
+## Set epsilons - these parameters govern the extent to which mixing is random or assortative. Note that we could break these out separately by for epsilon_age and epsilon_risk.
+epsilons <- fread("data/epsilons.csv")
+epsilons <- interpolate(breaks= epsilons$year, values = epsilons$epsilon)
+
+## Number of partners per year by age, sex, and risk
+partners <- fread("data/partners_per_year.csv")
+
+## Theta - parameter that governs the extent to which differences in reported number of sexual partners between males and females is male (1) or female (0) driven
+theta <- 0.5
+
+## Number of coital acts per partnership
+acts <- fread("data/acts_per_year.csv")
+
+## Per-act probability of HIV transmission by viral load of partner
+baseline <- 0.0006 ## Baseline probability of HIV transmission (VL < 1000 copies/mL)
+trans_probs <- fread("data/transmission_probabilities.csv")
+trans_probs[, beta := baseline * scalar]
+
+## Risk reduction for HIV-negative partner based on intervention usage
+risk_reduction <- fread("data/risk_reduction.csv")
+
+
+
