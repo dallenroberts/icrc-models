ece358f11afca7c8451ebedd5d14fbaba6dc75e2
unknown <dallenr@MXL24705MR_ICRC.clients.nebula2.washington.edu>
2015-07-15 17:44:50 -0700

    Started transmission function

---
+++ transmit.r
@@ -0,0 +1,15 @@
+##################################################
+## Allen Roberts
+## July 15, 2015
+## Description: Transmits infections based on lambda (risk of infection) and effect of interventions such as PrEP, circumcision, and condoms.
+##################################################
+
+transmit <- function(dt, lambdas) {
+  
+  ## Merge on risk reduction interventions.
+  
+  ## Adjust lambda based on probability of having the intervention (prevalence of the intervention) and risk reduction that the intervention applies
+  
+  ## Calculate new infections based on the adjusted lambdas
+  
+}
