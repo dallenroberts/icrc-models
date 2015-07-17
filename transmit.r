cdfc40bbd258c38f932ccf8f631adeec4067c9e2
unknown <dallenr@MXL24705MR_ICRC.clients.nebula2.washington.edu>
2015-07-17 16:13:13 -0700

    Added transmission code and code to keep track of incidence

--- transmit.r
+++ transmit.r
@@ -7,9 +7,38 @@
 transmit <- function(dt, lambdas) {
   
   ## Merge on risk reduction interventions.
+  dt[, psi := 1]
+  dt[condom == 1, psi := psi * risk_reduction[intervention == "condom", psi]]
+  dt[circ == 1 & male == 1, psi := psi * risk_reduction[intervention == "circ", psi]]
+  dt[prep == 1, psi := psi * risk_reduction[intervention == "prep", psi]]
   
-  ## Adjust lambda based on probability of having the intervention (prevalence of the intervention) and risk reduction that the intervention applies
+  ## Merge on lambda
+  setkey(dt, male, age, risk)
+  setkey(lambdas, male, age, risk)
+  dt[lambdas, lambda := lambda]
   
-  ## Calculate new infections based on the adjusted lambdas
+  ## Subtract from HIV negative population
+  dt[hiv == 0, diff := diff - count * lambda * psi]
+  
+  ## Keep track of incidence
+  new_infections <- dt[hiv == 0, list(time = tt, new_inf = sum(count * lambda * psi), inc_rate = lambda * psi), by = list(male, age, risk)]
+  setkey(new_infections, male, age, risk, time)
+  setkey(incidence, male, age, risk, time)
+  incidence[new_infections, c("horiz_infections", "rate") := list(new_inf, inc_rate)]
+  
+  ## Add to HIV positive population
+  new_hiv <- copy(dt)
+  new_hiv <- new_hiv[hiv == 0]
+  new_hiv[, c("hiv", "new_infections") := list(1, count * lambda * psi)]
+  
+  ## Seed new HIV infections in vl = 1 and cd4 = 1
+  new_hiv <- new_hiv[, list(new_infections = sum(new_infections)), by = list(hiv, age, male, risk, circ, prep, condom)]
+  new_hiv[, c("cd4", "vl") := 1]
+  setkeyv(new_hiv, all_keys)
+  setkeyv(dt, all_keys)
+  dt[new_hiv, diff := diff + new_infections]
+  
+  ## Clean up
+  dt[, c("psi", "lambda") := NULL]
   
 }
