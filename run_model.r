65fdd72c34350de11724d430984e6545ad7a2e04
unknown <dallenr@MXL24705MR_ICRC.clients.nebula2.washington.edu>
2015-07-17 16:13:59 -0700

    Sourced transmission function and added code to keep track of statistics and make plots

--- run_model.r
+++ run_model.r
@@ -34,21 +34,43 @@ source("progressDisease.r")
 source("seedInfections.r")
 source("riskAdjust.r")
 source("lambda_functions.r")
+source("transmit.r")
 
 ## Load input epidemiological parameters
 source("load_parameters.r")
 
 ## Initialize population matrix
-pop <- as.data.table(expand.grid(hiv, age, male, risk, cd4, vl, circ, prep, condom))
+pop <- as.data.table(expand.grid(sapply(all_keys, get)))
 setattr(pop, 'names', c("hiv", "age", "male", "risk", "cd4", "vl", "circ", "prep", "condom"))
 pop$count <- 0
 pop$diff <- 0
 
+## Data tables for statistics
+population <- as.data.table(expand.grid(hiv, age, male, risk, cd4, vl, circ, prep, condom, seq(year_start, year_end)))
+setattr(population, 'names', c("hiv", "age", "male", "risk", "cd4", "vl", "circ", "prep", "condom", "yy"))
+population[, pop_size := 0]
+
+
+# births <- as.data.table(expand.grid(age, male, seq(1, nsteps)))
+# setattr(births, 'names', c("age", "male", "time"))
+# 
+deaths <- as.data.table(expand.grid(hiv, age, male, risk, seq(1, nsteps)))
+setattr(deaths, 'names', c("hiv", "age", "male", "risk", "time"))
+deaths[, c("non_aids_deaths", "aids_deaths") := 0]
+
+incidence <- as.data.table(expand.grid(age, male, risk, seq(1, nsteps)))
+setattr(incidence, 'names', c("age", "male", "risk", "time"))
+incidence[, c("vert_infections", "horiz_infections", "rate") := 0]
+
+# art <- c(0, 1)
+# interventions <- as.data.table(expand.grid(age, male, circ, prep, condom, art, seq(1, nsteps)))
+
 ## Run model
 for(tt in 1:nsteps) {
   
+  # tt <- 1
   print(tt)
-  
+
   if(tt == 1) {
     ## Add intial populations.  Initially all are susceptible. 
     setkey(pop, hiv, age, male, cd4, vl, circ, prep, condom)
@@ -94,6 +116,12 @@ for(tt in 1:nsteps) {
   riskAdjust(pop)
   
   ## Calculate some statistics
+  ## Populations
+  if((year_start + (tt - 1) * tstep) %% 1 == 0) {
+    setkeyv(population, c("yy", all_keys))
+    setkeyv(pop, c("yy", all_keys))
+    population[pop, pop_size := count]
+  }
   
   # Increment time step
   tt <- tt + 1
@@ -101,4 +129,52 @@ for(tt in 1:nsteps) {
 }
 
 ## Make some plots
+population_size <- population[, list(total_size = sum(pop_size)), by = list(male, age, yy)]
+prevalence <- population[hiv == 1, list(size = sum(pop_size)), by = list(male, age, yy)]
+setkey(population_size, male, age, yy)
+setkey(prevalence, male, age, yy)
+prevalence[population_size, prev := size/total_size]
+prevalence[is.na(prev), prev := 0]
 
+risks <- population[, list(size = sum(pop_size)), by = list(risk, yy)]
+
+## Population size
+pop_plot <- ggplot(population_size, aes(x = yy, y = total_size)) +
+    geom_line(aes(colour = factor(male))) +
+    facet_wrap(~age)
+
+## HIV prevalence
+prev_plot <- ggplot(prevalence, aes(x = yy, y = prev)) +
+  geom_line(aes(group = male, colour = factor(male))) +
+  facet_wrap(~age) +
+  scale_y_continuous(limits = c(0, 1), breaks = seq(0, 1, by = 0.2))
+
+## HIV incidence
+incidence[, c("year", "year_exact") := list(floor(year_start + (time - 1) * tstep), floor(year_start + (time - 1) * tstep))]
+incidence[, infections := vert_infections + horiz_infections]
+
+inc_rate_plot <- ggplot(data = incidence, aes(x = year_exact, y = rate * 100)) +
+    geom_line(aes(colour = factor(male), linetype = factor(risk))) +
+    facet_wrap(~age)
+
+infections <- incidence[, list(yearly_infections = sum(infections)), by = list(age, male, year)]
+inc_plot <- ggplot(data = infections, aes(x = year, y = yearly_infections)) +
+    geom_line(aes(colour = factor(male))) +
+    facet_wrap(~age)
+
+## Deaths
+deaths[is.na(non_aids_deaths), non_aids_deaths := 0]
+deaths[is.na(aids_deaths), aids_deaths := 0]
+
+deaths[, c("year", "year_exact") := list(floor(year_start + (time - 1) * tstep), floor(year_start + (time - 1) * tstep))]
+deaths_data <- deaths[, list(background = sum(non_aids_deaths), hiv_relative = sum(aids_deaths), total = sum(non_aids_deaths + aids_deaths)), by = list(hiv, age, male, year)]
+
+deaths_data <- melt(deaths_data, id_vars = c("hiv", "age", "male", "year"), measure.vars = c("background", "hiv_relative", "total"), variable.name = "death_type", value.name = "deaths")
+deaths_plot <- ggplot(data = deaths_data, aes(x = year, y = deaths)) +
+    geom_line(aes(colour = factor(male), linetype = death_type)) +
+    facet_wrap(~age + hiv)
+
+hiv_deaths_data <- deaths_data[hiv == 1 & death_type == "hiv_relative"]
+hiv_deaths_plot <- ggplot(data = hiv_deaths_data, aes(x = year, y = deaths)) +
+  geom_line(aes(colour = factor(male))) +
+  facet_wrap(~age)
