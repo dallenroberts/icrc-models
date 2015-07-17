8944437d6695125b337c45894dd11f1db783eeca
unknown <dallenr@MXL24705MR_ICRC.clients.nebula2.washington.edu>
2015-07-17 16:10:40 -0700

    Adjusted parameters for time step and converted fertility and mortality to risks

--- load_parameters.r
+++ load_parameters.r
@@ -30,17 +30,21 @@ setkey(fert, cd4)
 fert[fert_coeffs, gamma := gamma * coeff]
 rm(fert_coeffs)
 
+## Adjust fertility by time step and convert to risk
+fert[, gamma := 1 - exp(-gamma * tstep)]
+
 ## Vertical transmission
 vert_trans <- fread("data/vertical_transmission.csv")
 vert_trans <- interpolate(breaks = vert_trans$year, values = vert_trans$vert)
 
 ## Background mortality (non-HIV) by age and sex
 back_mort <- fread("data/background_mortality.csv")
+back_mort[, mu := 1 - exp(-mu * tstep)] # Adjust for time step and convert to risk
 setkey(back_mort, age, male)
 
 ## HIV-relative mortality
 hiv_mort <- fread("data/hiv_mortality.csv")
-hiv_mort$hiv <- 1
+hiv_mort[, c("hiv", "alpha") := list(1, 1 - exp(-alpha * tstep))]
 setkey(hiv_mort, hiv, age, cd4)
 
 ## Disease progression - cd4_duration and vl_duration are average duration spent in that CD4 or VL category (respectively) in years
@@ -64,12 +68,15 @@ epsilons <- interpolate(breaks= epsilons$year, values = epsilons$epsilon)
 
 ## Number of partners per year by age, sex, and risk
 partners <- fread("data/partners_per_year.csv")
+## Adjust by time-step
+partners[, partners := partners * tstep]
 
 ## Theta - parameter that governs the extent to which differences in reported number of sexual partners between males and females is male (1) or female (0) driven
 theta <- 0.5
 
 ## Number of coital acts per partnership
-acts <- fread("data/acts_per_year.csv")
+acts <- fread("data/acts_per_year.csv") 
+acts[, acts := acts * tstep] 
 
 ## Per-act probability of HIV transmission by viral load of partner
 baseline <- 0.0006 ## Baseline probability of HIV transmission (VL < 1000 copies/mL)
@@ -93,7 +100,7 @@ betas[acts, acts := acts]
 ## Note that the vl variable is actually the viral load of the partner
 setnames(betas, "vl", "vl_p")
 
-## Calculate transmission probability per partnership per year
+## Calculate transmission probability per partnership per time step
 betas[, transmission_risk := 1 - (1 - chi) ^ acts]
 
 ## Risk reduction for HIV-negative partner based on intervention usage
